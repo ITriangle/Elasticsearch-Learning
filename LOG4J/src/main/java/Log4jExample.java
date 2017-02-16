8c48c1e8b8ae7a5a12aac68f375d6b742957b88d
1968155152@qq.com <1968155152@qq.com>
2017-02-16 18:33:01 +0800

    WritetoFile

--- LOG4J/src/main/java/Log4jExample.java
+++ LOG4J/src/main/java/Log4jExample.java
@@ -1,4 +1,5 @@
 import org.apache.log4j.Logger;
+import org.junit.Test;
 
 import java.io.IOException;
 
@@ -11,6 +12,16 @@ public class Log4jExample {
     static Logger logger = Logger.getLogger(Log4jExample.class.getName());
 
     public static void main(String[] args) throws IOException {
+        // 记录debug级别的信息
+        logger.debug("This is debug message.");
+        // 记录info级别的信息
+        logger.info("This is info message.");
+        // 记录error级别的信息
+        logger.error("This is error message.");
+    }
+
+    @Test
+    public void test(){
         // 记录debug级别的信息
         logger.debug("This is debug message.");
         // 记录info级别的信息
